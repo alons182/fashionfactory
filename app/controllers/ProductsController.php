362062f87412d3c5150530c95022a6a41a6b01b7
alonso <alons182@hotmail.com>
2014-06-12 11:34:34 -0600

    docs op

--- app/controllers/ProductsController.php
+++ app/controllers/ProductsController.php
@@ -8,18 +8,18 @@ use Fashion\Repos\Photo\PhotoRepository;
 
 class ProductsController extends \BaseController {
 
-	
-	protected $productRepository;
-	protected $categoryRepository;    
-	protected $photoRepository;
-	
-	function __construct(ProductRepository $productRepository, CategoryRepository $categoryRepository, PhotoRepository $photoRepository)
-	{
-		$this->productRepository = $productRepository;
-		$this->categoryRepository = $categoryRepository;
-		$this->photoRepository = $photoRepository;
-		$this->limit = 15;
-	}
+
+    protected $productRepository;
+    protected $categoryRepository;
+    protected $photoRepository;
+
+    function __construct(ProductRepository $productRepository, CategoryRepository $categoryRepository, PhotoRepository $photoRepository)
+    {
+        $this->productRepository = $productRepository;
+        $this->categoryRepository = $categoryRepository;
+        $this->photoRepository = $photoRepository;
+        $this->limit = 15;
+    }
 
     /**
      * Display a listing of the resource.
@@ -28,31 +28,31 @@ class ProductsController extends \BaseController {
      * @param $category
      * @return Response
      */
-	public function index($category)
-	{
-		
-		$category = $this->categoryRepository->getCategories()->SearchSlug($category)->firstOrFail();
-				 
-		$products = $category->products()->with('categories')->where('published', '=', 1)->paginate($this->limit);
-		
-		return  View::make('products.index')->withProducts($products)->withCategory($category); 
-	}
+    public function index($category)
+    {
 
-	/**
-	 * Display a listing of the resource.
-	 * GET /products
-	 *
-	 * @return Response
-	 */
-	public function search()
-	{
-		
-		$search = Input::all();
-		
-		$products = Search::products($search);
-		
-		return  View::make('products.index')->withProducts($products)->withSearch($search['q']); 
-	}
+        $category = $this->categoryRepository->getCategories()->SearchSlug($category)->firstOrFail();
+
+        $products = $category->products()->with('categories')->where('published', '=', 1)->paginate($this->limit);
+
+        return View::make('products.index')->withProducts($products)->withCategory($category);
+    }
+
+    /**
+     * Display a listing of the resource.
+     * GET /products
+     *
+     * @return Response
+     */
+    public function search()
+    {
+
+        $search = Input::all();
+
+        $products = Search::products($search);
+
+        return View::make('products.index')->withProducts($products)->withSearch($search['q']);
+    }
 
     /**
      * Display the specified resource.
@@ -63,19 +63,18 @@ class ProductsController extends \BaseController {
      * @internal param int $id
      * @return Response
      */
-	public function show($category, $product)
-	{
-		$category = $this->categoryRepository->getCategories()->SearchSlug($category)->firstOrFail();
-		
-		$product = $category->products()->SearchSlug($product)->firstOrFail();
+    public function show($category, $product)
+    {
+        $category = $this->categoryRepository->getCategories()->SearchSlug($category)->firstOrFail();
 
-		$relateds = $this->productRepository->relateds($product);
+        $product = $category->products()->SearchSlug($product)->firstOrFail();
 
-		$photos = $this->photoRepository->getPhotos($product->id);
-		
-		return  View::make('products.show')->withProduct($product)->withRelateds($relateds)->withCategory($category)->withPhotos($photos); 
-	}
+        $relateds = $this->productRepository->relateds($product);
 
-	
+        $photos = $this->photoRepository->getPhotos($product->id);
+
+        return View::make('products.show')->withProduct($product)->withRelateds($relateds)->withCategory($category)->withPhotos($photos);
+    }
+
 
 }
